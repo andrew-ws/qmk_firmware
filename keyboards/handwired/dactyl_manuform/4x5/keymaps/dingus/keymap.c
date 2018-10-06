ff23bc874e39a56f48067de2c77dcee862be695d
Andrew <asherman50@gmail.com>
2018-10-06 19:26:41 -0400

    better keymap

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/dingus/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/dingus/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
   KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, \
               KC_PAUSE, KC_GRV,                                                  KC_QUOT, KC_ENT,                    \
-                                     KC_LCTRL, RAISE,     LOWER,   KC_LALT,                                          \
-                                     KC_BSPC,  KC_TAB,    KC_SPC,  KC_RALT,                                            \
+                                     KC_LCTRL, KC_LSFT,   LOWER,   KC_LALT,                                          \
+                                     KC_BSPC,  KC_TAB,    KC_SPC,  RAISE,                                            \
                                      KC_ESC,   KC_MENU,   KC_LGUI, KC_SLSH
 ),
 
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  ____, ____,    KC_UP,  ____,  ____,                          KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP,   \
-  ____, KC_LEFT, KC_DOWN, KC_RIGHT,  ____,                          KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, \
+  ____, ____,    KC_UP,  ____,  ____,                         KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP,   \
+  ____, KC_LEFT, KC_DOWN, KC_RIGHT,  ____,                    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGDOWN, \
   ____, ____,    ____,  ____,  ____,                          KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES,  KC_PIPE,   \
         ____,    ____,                                                          ____,    ____,                \
                                    ____, ____,  ____, ____,                                                 \
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* Lower
  * ,----------------------------------,                             ,----------------------------------,
- * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
- * |------+------+------+------+------|                             |-------------+------+------+------|
  * |  1   |  2   |  3   |  4   |  5   |                             |  6   |  7   |  8   |  9   |  10  |
+ * |------+------+------+------+------|                             |-------------+------+------+------|
+ * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|                             |------|------+------+------+------|
  * |  !   |  @   |  #   |  $   |  %   |                             |  ^   |  &   |  *   |  (   |  )   |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
   KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
-  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, \
+  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_GRV,   KC_MINS,  KC_EQL,  KC_LBRC, KC_RBRC, \
+  KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,                           KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PGDN, \
            KC_F11, KC_F12,                                                               ____,    ____,             \
                                              ____, ____,  ____, ____,                                               \
                                              ____, ____,  ____, ____,                                               \
